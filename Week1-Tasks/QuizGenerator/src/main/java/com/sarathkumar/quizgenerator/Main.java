a9655c694e79c605960c5147e5be32ceb8280583
SARATHKUMAR <sarathkumar.t2003@gmail.com>
2024-12-08 16:49:01 +0530

    Delete Week1-Tasks/QuizGenerator directory

--- Week1-Tasks/QuizGenerator/src/main/java/com/sarathkumar/quizgenerator/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sarathkumar.quizgenerator;
-
-public class Main {
-    public static void main(String[] args) {
-        QuizManager quizManager = new QuizManager();
-        quizManager.start();
-
-    }
-}
